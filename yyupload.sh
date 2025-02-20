b05bc8b255d455705d9d31819d5d9b6819bc2078
wangbo1 <wangbo1@cmss.chinamobile.com>
2025-02-20 17:51:24 +0800

    d3064d9a

--- yyupload.sh
+++ yyupload.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+git config --global http.proxy "http://172.18.55.61:20171/"
+git config --global https.proxy "http://172.18.55.61:20171/"
 git pull
 git add .
 LENGTH=8
@@ -6,3 +8,5 @@ RANDOM_STRING=$(date +%s%N | md5sum | head -c $LENGTH)
 git commit -m "$RANDOM_STRING"
 git pull
 git push
+git config --global --unset http.proxy
+git config --global --unset https.proxy
